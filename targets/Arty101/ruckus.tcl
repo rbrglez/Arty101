75224ce8afb57e5ccbec765f10c2efb6c741a2cc
rene brglez <rene.brglez@cosylab.com>
2022-12-29 23:04:47 +0100

    Removed Obsolete module (ArtyIo). It is replaced by GeneralIO module

--- targets/Arty101/ruckus.tcl
+++ targets/Arty101/ruckus.tcl
@@ -10,11 +10,6 @@ loadSource      -dir  "$::DIR_PATH/src/"
 loadConstraints  -dir  "$::DIR_PATH/src/"
 
 # Load modules
-loadRuckusTcl "$::env(TOP_DIR)/modules/ArtyIo/ArtyButtonInputs"
-loadRuckusTcl "$::env(TOP_DIR)/modules/ArtyIo/ArtySwitchInputs"
-loadRuckusTcl "$::env(TOP_DIR)/modules/ArtyIo/ArtyLedOutputs"
-loadRuckusTcl "$::env(TOP_DIR)/modules/ArtyIo/ArtyRgbLedOutputs"
-
 loadRuckusTcl "$::env(TOP_DIR)/modules/GeneralIo"
 loadRuckusTcl "$::env(TOP_DIR)/modules/Arty101Io"
 loadRuckusTcl "$::env(TOP_DIR)/modules/KeypadDecoder"
